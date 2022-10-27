9cd2e1be884803befb0be23c9ebb3ada887e8577
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-10-27 11:03:56 +0200

    added functionality of checkbox

--- index.js
+++ index.js
@@ -10,23 +10,38 @@ const Task = (props) => {
         doneSign = '✓'; 
     } 
     
-        return `
-    <div class="task">
-        <div class="task__body">
-          <div class="task__name">${name}</div>
-          <div class="task__due">${due}</div>
+    return `
+        <div class="task">
+            <div class="task__body">
+                <div class="task__name">${name}</div>
+                <div class="task__due">${due}</div>
+            </div>
+            <div class=${doneClass}>${doneSign}</div>
         </div>
-        <div class=${doneClass}>${doneSign}</div>
-      </div>
     `
-}
+};
 
 const renderTasks = (items) => {
     document.querySelector('.todo__tasks').innerHTML = items
     .map((item) => Task(item))
     .join('') 
-}
+};
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
 .then((response) => response.json())
 .then((data) => renderTasks(data));
+
+const checkElm = document.getElementById('checkbox-undone');
+checkElm.addEventListener('click', (event) => {
+  if (checkElm.checked == true) { 
+    fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=false')
+    .then((response) => response.json())
+    .then((data) => renderTasks(data));
+    } 
+
+  else {
+    fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+    .then((response) => response.json())
+    .then((data) => renderTasks(data));
+    }
+});
