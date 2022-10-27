545e79d98a0baded455590370196803f8548f273
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-10-27 10:14:07 +0200

    added component Task

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('funguju!');
+
+const Task = (props) => {
+    {name, due, done} = props;
+
+    let doneClass = '';
+    const doneSign = '';
+    if (done) {
+        doneClass = 'task__done';
+        doneSign = '✓';
+        
+    } 
+    
+        return `
+    <div class="task">
+        <div class="task__body">
+          <div class="task__name">${name}</div>
+          <div class="task__due">${due}</div>
+        </div>
+        <div class=${doneClass}>${doneSign}</div>
+      </div>
+    `
+}
